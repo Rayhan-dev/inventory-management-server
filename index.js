383bda0576df56a8bd31bfd98313b53f5b1a7e83
rayhan-dev <mohammodrayhan444@gmail.com>
2022-04-30 19:26:36 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aivdt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const bookCollection = client.db("inventoryDB").collection("books");
+      
+    //GET BOOKS 
+    app.get("/books",async(req, res) => {
+      const query = {};
+      const cursor = bookCollection.find(query);
+      const books =await cursor.toArray();
+      res.send(books);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
